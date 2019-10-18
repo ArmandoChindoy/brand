bdcba0cd2e405723e2abd9b81d3637c1c061bfc7
Richard <rickcalderon07@gmail.com>
2019-10-18 10:28:08 -0500

    Bug fixing in the inputs to show the values in the textarea

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,27 +1,39 @@
-var orign = document.getElementById("origen").value;
-var destiny = document.getElementById("destino").value;
-var startDate = document.getElementById("fecha_ida").value;
-var finishDate = document.getElementById("fecha_vuelta").value;
-var rooms = document.getElementById("habitaciones").value;
-var persons = document.getElementById("personas").value;
+var orign;
+var destiny;
+var startDate;
+var finishDate;
+var rooms;
+var persons;
 var textarea = document.getElementById("text_area_search_section");
 var button = document.getElementsByClassName("search_button");
 
 
+function getValues(){
+orign = document.getElementById('origen').value;
+destiny= document.getElementById("destino").value;
+startDate= document.getElementById("fecha_ida").value;
+finishDate= document.getElementById("fecha_vuelta").value;
+rooms = document.getElementById("habitaciones").value;
+persons = document.getElementById("personas").value;
+}
+
+
 class peticion {
-    constructor(origen, destino, fecha_ida, fecha_vuelta, habitaciones, personas) {
+    constructor(origen, destino, fecha_ida, fecha_vuelta,habitaciones,personas) {
         this.origen = origen;
         this.destino = destino;
         this.fecha_ida = fecha_ida;
-        this.fecha_vuelta = fecha_vuelta;
+        this.fecha_vuelta = fecha_vuelta;   
         this.habitaciones = habitaciones;
         this.personas = personas
     }
 }
-var peticiones = new Array;
-peticiones.push(new peticion(orign, destiny, startDate, finishDate, parseInt(rooms), parseInt(persons)));
 
 function show() {
+    getValues();
+    var peticiones = new Array;
+peticiones.push(new peticion(orign, destiny, startDate, finishDate, parseInt(rooms), parseInt(persons)));
+
     peticiones.forEach(element => {
         textarea.value = element.origen + " " + element.destino + " " + element.fecha_ida + " " + element.fecha_vuelta + " " + element.habitaciones + " " + element.personas;
     });
